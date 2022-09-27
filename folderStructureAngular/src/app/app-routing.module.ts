73f8b8f19a31b19d91bb465e1505110a8a0eb5de
Sreekanth Mandala <smandala@ariqt.com>
2022-09-28 02:04:37 +0530

    added owlcarosel and home

--- folderStructureAngular/src/app/app-routing.module.ts
+++ folderStructureAngular/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  { path: 'owlcarousel2', loadChildren: () => import('./components/owlcarousel2/owlcarousel2.module').then(m => m.Owlcarousel2Module) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
